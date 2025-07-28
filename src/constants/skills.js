72986bb01ef2f64f88f3d6d3e323d6c3f86ef453
Roshan Jain <roshanjain.220407@gmail.com>
2025-07-28 16:47:44 +0530

    Fixed bugs and rollbacked

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,20 +1,19 @@
-
-
-
 export const SKILLS = [
-    { name: "HTML", logo: "../assets/skills/html.svg", color: "#CE653B" ,  },
-    { name: "CSS", logo: "../assets/skills/Css.svg", color: "#264DE4" },
-    { name: "JavaScript", logo: "../assets/skills/Js.svg", color: "#FFD600" },
-    { name: "React", logo: "../assets/skills/React.svg", color: "#6563FF" },
-    { name: "GSAP", logo: "../assets/skills/gsap.svg", color: "#FFFCE1" },
-    { name: "Tailwind", logo: "../assets/skills/Tailwind Css.svg", color: "#5DE5E9" },
-    { name: "Git", logo: "../assets/skills/Git.svg", color: "#F34F29" },
-    { name: "Github", logo: "../assets/skills/github.svg", color: "#FFF" },
-    { name: "MongoDB", logo: "../assets/skills/Mongodb.svg", color: "#45A538" },
-    { name: "Java", logo: "../assets/skills/Java.svg", color: "#EA2D2E" },
-    { name: "Figma", logo: "../assets/skills/Figma.svg", color: "#FF4611" },
-    { name: "After Effects", logo: "../assets/skills/ae.svg", color: "#D8A1FF" },
-    { name: "Premiere Pro", logo: "../assets/skills/Premiere.svg", color: "#E579FF" },
-    { name: "Photoshop", logo: "../assets/skills/ps.svg", color: "#A0CBFF" },
-  ];
-  
+  { name: "HTML", logo: "../assets/skills/html.svg", color: "#CE653B" ,  },
+  { name: "CSS", logo: "../assets/skills/Css.svg", color: "#264DE4" },
+  { name: "JavaScript", logo: "../assets/skills/Js.svg", color: "#FFD600" },
+  { name: "React", logo: "../assets/skills/React.svg", color: "#6563FF" },
+  { name: "GSAP", logo: "../assets/skills/gsap.svg", color: "#FFFCE1" },
+  { name: "Tailwind", logo: "../assets/skills/Tailwind Css.svg", color: "#5DE5E9" },
+  { name: "Git", logo: "../assets/skills/Git.svg", color: "#F34F29" },
+  { name: "Github", logo: "../assets/skills/github.svg", color: "#FFF" },
+  { name: "Scrolltrigger", logo: "../assets/skills/scrolltrigger.webp", color: "#FFF" },
+  { name: "Lenis", logo: "../assets/skills/lenis.webp", color: "#FFF" },
+  { name: "Wordpress", logo: "../assets/skills/wordpress.webp", color: "#FFF" },
+  { name: "MongoDB", logo: "../assets/skills/Mongodb.svg", color: "#45A538" },
+  { name: "Java", logo: "../assets/skills/Java.svg", color: "#EA2D2E" },
+  { name: "Figma", logo: "../assets/skills/Figma.svg", color: "#FF4611" },
+  { name: "After Effects", logo: "../assets/skills/ae.svg", color: "#D8A1FF" },
+  { name: "Premiere Pro", logo: "../assets/skills/Premiere.svg", color: "#E579FF" },
+  { name: "Photoshop", logo: "../assets/skills/ps.svg", color: "#A0CBFF" },
+];
