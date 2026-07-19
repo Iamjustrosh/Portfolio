0db9a87d5199e3413e9a405b0502c2f8fbe94a25
Roshan Jain <roshanjain.220407@gmail.com>
2026-07-19 09:21:48 +0530

    feat: add navbar link and integrate Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import {Clarity} from '@microsoft/clarity'
+
+
+
+Clarity.init(import.meta.env.VITE_PROJECT_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
